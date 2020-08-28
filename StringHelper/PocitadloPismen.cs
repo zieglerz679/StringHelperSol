75353629a18f0e709ee0c3b1bea7b793289bd8b3
Zdenek <zieglerz679@gmail.com>
2020-08-28 09:02:11 +0200

    Add project files.

---
+++ StringHelper/PocitadloPismen.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringHelper
+{
+    public class PocitadloPismen
+    {
+        private string text;
+        public string Text
+        {
+            get { return text; }
+            set { 
+                text = value;
+                seznam.Clear();
+            }
+        }
+
+        private readonly Dictionary<char, int> seznam = new Dictionary<char, int>();
+
+        public int this[char c]
+        {
+            get
+            {
+                if (seznam.ContainsKey(c))
+                    return seznam[c];
+
+                int pocet = 0;
+                foreach (char item in Text)
+                {
+                    if (item == c)
+                        pocet++;
+                }
+                seznam.Add(c, pocet);
+                return pocet;
+            }
+        }
+
+        public void Akce() { }
+    }
+}
---
+++ StringHelperTests/PocitadloPismenTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringHelper.Tests
+{
+    [TestClass()]
+    public class PocitadloPismenTests
+    {
+        [TestMethod()]
+        public void IndexerTest()
+        {
+            PocitadloPismen pocitadlo = new PocitadloPismen();
+            pocitadlo.Text = "Máma nemá ráda maso.";
+
+            int expected = 3;
+            int result = pocitadlo['a'];
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
